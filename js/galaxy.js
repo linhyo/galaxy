b8404cd7eccc89d1f8fd2597ce47ed119833f0a3
Minh Hoang <minh.le@jmango360.com>
2016-03-24 03:28:02 +0700

    Edit slide in decor page

--- js/galaxy.js
+++ js/galaxy.js
@@ -125,5 +125,25 @@
     }
   });
 
+  //Slick for decor name
+  var listDecorName = ['painting1', 'painting2', 'painting3', 'painting4', 'painting5', 'painting6', 'painting7', 'painting8'];
+  var filterListDecor = function (name) {
+    $('#' + name).slick({
+      nextArrow: $('.'+ name +' .nextArrow'),
+      prevArrow: $('.'+ name +' .prevArrow')
+    });
+  };
+  _.forEach(listDecorName, function(name){
+    filterListDecor(name);
+  });
+  $('a[data-toggle=tab]').each(function () {
+    var $this = $(this);
+    $this.on('shown.bs.tab', function () {
+      _.forEach(listDecorName, function(name){
+        filterListDecor(name);
+      });
+    });
+  });
+
 })();
 

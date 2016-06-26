01ae51c534ed5c4c6652fa830170b6aea90d7434
Minh Hoang <minh.le@jmango360.com>
2016-06-26 12:37:01 +0700

    Add effect for landing page

--- js/landing-luckypainter.js
+++ js/landing-luckypainter.js
@@ -14,4 +14,94 @@ $(document).ready(function () {
     }
   );
   wow.init();
+
+  var tab1 = $('#lucky-painter-header');
+  var tab2 = $('#lucky-painter-header-content');
+  var tab3 = $('#lucky-painter-header-register');
+  tab2.hide();
+  tab3.hide();
+  $('.scroll-block').slimScroll({
+    height: '340px'
+  });
+  tab1.find('.btn-content-event').click(function (e) {
+    e.preventDefault();
+    $(this).parents('#lucky-painter-header').animate({width: "100%", left: '3%', 'z-index': '1'}, '500')
+      .animate({left: '-95%'}, '500');
+
+    setTimeout(function () {
+      tab1.hide();
+      tab2.show();
+      tab2.css('left', '105%')
+        .css('width', '100%')
+        .css('position', 'relative');
+      tab2.animate({left: '5%'}, '500')
+        .animate({width: '100%', left: '0'}, '500');
+    }, '500');
+  });
+
+  tab1.find('.btn-register').click(function (e) {
+    e.preventDefault();
+    $(this).parents('#lucky-painter-header').animate({width: "100%", left: '3%', 'z-index': '1'}, '500')
+      .animate({left: '115%'}, '500');
+
+    setTimeout(function () {
+      tab1.hide();
+      tab3.show();
+      tab3.css('left', '-95%')
+        .css('width', '100%')
+        .css('position', 'relative');
+      tab3.animate({left: '5%'}, '500')
+        .animate({width: '100%', left: '0'}, '500');
+    }, '500');
+  });
+
+  tab2.find('.btn-prev-block1').click(function (e) {
+    e.preventDefault();
+    $(this).parents('#lucky-painter-header-content').animate({width: "100%", left: '3%'}, '500')
+      .animate({left: '115%'}, '500');
+
+    setTimeout(function () {
+      tab2.hide();
+      tab1.show();
+      tab1.css('left', '-95%')
+        .css('width', '100%')
+        .css('z-index', '1')
+        .css('position', 'relative');
+      tab1.animate({left: '5%'}, '500')
+        .animate({width: '100%', left: '0'}, '500');
+    }, '500');
+  });
+
+  tab3.find('.btn-prev-block2').click(function (e) {
+    e.preventDefault();
+    $(this).parents('#lucky-painter-header-register').animate({width: "100%", left: '3%'}, '500')
+      .animate({left: '-95%'}, '500');
+
+    setTimeout(function () {
+      tab3.hide();
+      tab1.show();
+      tab1.css('left', '105%')
+        .css('width', '100%')
+        .css('z-index', '1')
+        .css('position', 'relative');
+      tab1.animate({left: '5%'}, '500')
+        .animate({width: '100%', left: '0'}, '500');
+    }, '500');
+  });
+
+  $('.btn-submit').click(function(e){
+    e.preventDefault();
+    $(this).parents('.register-form').animate({width: "100%", left: '3%'}, '500')
+      .animate({left: '-95%'}, '500');
+
+    setTimeout(function () {
+      $('.register-form').hide();
+      $('.register-success').show();
+      $('.register-success').css('left', '105%')
+        .css('width', '100%')
+        .css('position', 'relative');
+      $('.register-success').animate({left: '5%'}, '500')
+        .animate({width: '100%', left: '0'}, '500');
+    }, '500');
+  });
 });
